35729ebe6051309dd4713e9690bda08b6f5b00cc
azhar <azharrauf1971@gmail.com>
2019-09-07 01:16:14 -0400

    made some changes

--- src/EmployeeOne.java
+++ src/EmployeeOne.java
@@ -1,43 +1,44 @@
 import java.util.*;
 
 public class EmployeeOne {
-        public static void main(String[] args) {
-            List<Employee> employees = new ArrayList<>();
-           Map<Integer, Employee> employeemap = new HashMap<>();
-//            employees.add(new Employee(1001, "azhar", 40000.0));
-//        employees.add(new Employee(1002, "shams", 50000.0));
-//        employees.add(new Employee(1003, "ali", 60000.0));
-//        employees.add(new Employee(1004, "hadi", 70000.0));
-//        employees.add(new Employee(1005, "basit", 80000.0));
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+        //Map<Integer, Employee> employeemap = new HashMap<>();
+        employees.add(new Employee(1001, "azhar", 40000.0));
+        employees.add(new Employee(1002, "shams", 50000.0));
+        employees.add(new Employee(1003, "ali", 60000.0));
+        employees.add(new Employee(1004, "hadi", 70000.0));
+        employees.add(new Employee(1005, "basit", 80000.0));
 
-             employeemap.put(1001,new Employee(1001,"shah",45000.00));
-            employeemap.put(1002,new Employee(1001,"sh",45000.00));
-            employeemap.put(1003,new Employee(1001,"sah",45000.00));
-            employeemap.put(1004,new Employee(1001,"hah",45000.00));
-            employeemap.put(1005,new Employee(1001,"s",45000.00));
+//             employeemap.put(1001,new Employee(1001,"shah",45000.00));
+//            employeemap.put(1002,new Employee(1001,"sh",45000.00));
+//            employeemap.put(1003,new Employee(1001,"sah",45000.00));
+//            employeemap.put(1004,new Employee(1001,"hah",45000.00));
+//            employeemap.put(1005,new Employee(1001,"s",45000.00));
+//            This is new one for Jenkins Test
+        Scanner scanner = new Scanner(System.in);
+        int exit = 0;
+        while (exit == 0) {
+            System.out.println("Please enter Employee Id");
+            int empId = Integer.parseInt(scanner.nextLine());
+            if (empId == 0) {
+                exit = 1;
 
-            Scanner scanner = new Scanner(System.in);
-            int exit = 0;
-            while (exit == 0) {
-                System.out.println("Please enter Employee Id");
-                int empId = Integer.parseInt(scanner.nextLine());
-                if(empId==0){
-                    exit=1;
 
-
-//                    for (Employee emp : employees){
-//                        if(emp.getEmployeeId()==empId){
-//                            System.out.println(emp);
-//                        }
-                    }else {
-                System.out.println(employeemap.get(empId));
+                for (Employee emp : employees) {
+                    if (emp.getEmployeeId() == empId) {
+                        System.out.println(emp);
+                    }
                 }
+//                    else {
+//                System.out.println(employeemap.get(empId));
+//                }
 
             }
         }
 
 
-         static class Employee {
+        class Employee {
             private int employeeId;
             private String name;
             private double salary;
